5259d62719e567af0b36c5c910c28cc81b62832d
Doriano De Iaco <dorianblack97@gmail.com>
2022-05-06 12:50:32 +0200

    Error on loading home page

--- ProgettoCinema/ProgettoCinema.WebClient/Controller/CinemaController.cs
+++ ProgettoCinema/ProgettoCinema.WebClient/Controller/CinemaController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using ProgettoCinema.API.Data;
+using ProgettoCinema.ClientWeb.Data;
 using ProgettoCinema.Domain;
 
 namespace ProgettoCinema.WebClient.Controller

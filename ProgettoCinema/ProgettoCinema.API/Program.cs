22d6a5be6d7daf004599c1549a89050e47c17483
Doriano De Iaco <dorianblack97@gmail.com>
2022-05-06 11:55:53 +0200

    Add Repository in IoC

--- ProgettoCinema/ProgettoCinema.API/Program.cs
+++ ProgettoCinema/ProgettoCinema.API/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ProgettoCinema.API.Data;
+using ProgettoCinema.API.Repository;
+using ProgettoCinema.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<GenericRepository<Cinema>>();
+builder.Services.AddScoped<GenericRepository<Film>>();
+builder.Services.AddScoped<GenericRepository<Biglietto>>();
+builder.Services.AddScoped<GenericRepository<Spettatore>>();
+builder.Services.AddScoped<GenericRepository<SalaCinematografica>>();
+builder.Services.AddScoped<GenericRepository<GenereFilm>>();
 builder.Services.AddDbContext<CinemaDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDatabase")));
 
